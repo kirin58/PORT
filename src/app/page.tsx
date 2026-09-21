0c5ca47b9afcd9110c8d196b69a0c7c2764d16d2
PT <peepeerapat3105@gmail.com>
2026-09-22 02:17:02 +0700

    revert: remove snake mini-game section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import About from "@/components/About";
 import Skills from "@/components/Skills";
 import Projects from "@/components/Projects";
 import Experience from "@/components/Experience";
-import MiniGame from "@/components/MiniGame";
 import Contact from "@/components/Contact";
 
 export default function Home() {
@@ -17,7 +16,6 @@ export default function Home() {
         <Skills />
         <Projects />
         <Experience />
-        <MiniGame />
         <Contact />
       </main>
     </div>
